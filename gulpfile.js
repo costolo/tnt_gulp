4dbfcc5e775dca246bb9e3935beeb1295b22f79e
Ben <ben.costolo@gmail.com>
2016-04-27 12:37:08 -0400

    clean up variable declarations

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,21 @@
 // fill out the config.js file wit the appropriate properties
 // to specify an alternate config file (i.e. a config file for challenger B)
 //     - gulp --config configB.js
-var gulp = require('gulp');
-var gulpIf = require('gulp-if');
-var jsValidate = require('gulp-jsvalidate');
-var babel = require('gulp-babel');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var minCss = require('gulp-clean-css');
-var argv = require('yargs').argv;
-var config = argv.config ? require('./' + argv.config) : require('./config');
-var fs = require('fs');
+var gulp        = require('gulp');
+var gulpIf      = require('gulp-if');
+var jsValidate  = require('gulp-jsvalidate');
+var babel       = require('gulp-babel');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+var concat      = require('gulp-concat');
+var minCss      = require('gulp-clean-css');
+var argv        = require('yargs').argv;
+var fs          = require('fs');
 var prependFile = require('prepend-file');
-var del = require('del');
-
-var jsFile = config.jsFile ? config.jsFile.split('.js')[0] + '.min.js' : false;
-var cssFile = config.cssFile ? config.cssFile.split('.css')[0] + '.min.css' : false;
+var del         = require('del');
+var config      = argv.config ? require('./' + argv.config) : require('./config');
+var jsFile      = config.jsFile ? config.jsFile.split('.js')[0] + '.min.js' : false;
+var cssFile     = config.cssFile ? config.cssFile.split('.css')[0] + '.min.css' : false;
 
 gulp.task('minifyCSS', function() {
   if (cssFile) {
