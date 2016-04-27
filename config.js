0179dc3f9c51591f4c64cf861f8f99162b398dc7
Ben <ben.costolo@gmail.com>
2016-04-27 12:08:48 -0400

    add configuration file

---
+++ config.js
@@ -0,0 +1,21 @@
+var config = {};
+
+//name of css file
+config.cssFile = 'challA.css';
+
+//name of js file
+config.jsFile = 'challA.js';
+
+//name of challenger
+config.challenger = 'challA.html';
+
+//enable ES2015 transpiling
+config.babel = true;
+
+//do not minify files
+config.verbose = true;
+
+//do not delete minified files
+config.preserveMinFiles = false;
+
+module.exports = config;
